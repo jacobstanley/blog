6d645fc5d583239fc2c13b940046ec52c813884c
Jacob Stanley <jacob@stanley.io>
2011-08-22 02:50:01 +0800

    Completed supporting content for posts

---
+++ about.markdown
@@ -0,0 +1,24 @@
+---
+title: About
+---
+
+About
+=====
+
+<img
+src="http://gravatar.com/avatar/3aae84d7d47764bed1dae8ba398b7f97?size=128"
+alt="Jacob Stanley" title="Jacob Stanley" class="about-picture" />
+
+My name is Jacob Stanley and I'm a software developer from Perth,
+Western Australia. I work on a development team which mainly uses C# and
+C++ to build software for the [offshore survey
+industry](http://www.fugro.com.au/divisions/survey/offshore-survey). I
+have a keen interesting in functional, side effect free and parallel
+programming, so [Haskell](http://www.haskell.org) has become my
+favourite language to use when I'm not at work. This blog contains my
+random musings on various technical subjects.
+
+## My profiles on other sites
+
+- [CV (on Stack Overflow Careers)](http://careers.stackoverflow.com/jacobstanley)
+- [Stack Overflow](http://stackoverflow.com/users/72821/jacob-stanley)
--- hakyll.hs
+++ hakyll.hs
@@ -19,11 +19,12 @@ import qualified Hakyll as H
 ------------------------------------------------------------------------
 
 main :: IO ()
-main = hakyll $ do
+main = hakyllWith configuration $ do
 
     -- Images and static files
     ["favicon.ico"] --> copy
     ["robots.txt"]  --> copy
+    ["blank.html"]  --> copy
     ["img/**"]      --> copy
     ["files/**"]    --> copy
     ["js/**"]       --> copy
@@ -42,19 +43,29 @@ main = hakyll $ do
 
     -- RSS
     match  "atom.xml" $ route idRoute
-    create "atom.xml" $ requireAll_ "posts/*" >>> renderAtom feedCfg
+    create "atom.xml" $ requireAll_ "posts/*" >>> renderAtom feedConfiguration
   where
     -- Useful combinator here
     xs --> f = mapM_ (\p -> match p $ f) xs
 
 numRecentPosts :: Int
-numRecentPosts = 10
+numRecentPosts = 3
 
 siteRoot :: String
 siteRoot = "http://jacob.stanley.io"
 
-feedCfg :: FeedConfiguration
-feedCfg = FeedConfiguration
+configuration :: HakyllConfiguration
+configuration = defaultHakyllConfiguration
+    { deployCommand =
+        "rsync -ave 'ssh' " ++ local ++ "/* " ++ server ++ ":" ++ remote
+    }
+  where
+    server = "jystic_jacobstanley@ssh.phx.nearlyfreespeech.net"
+    remote = "/home/public"
+    local  = destinationDirectory defaultHakyllConfiguration
+
+feedConfiguration :: FeedConfiguration
+feedConfiguration = FeedConfiguration
     { feedTitle       = "Jacob Stanley :: IO Blog"
     , feedDescription = "Ceiling cat is watching you unsafePerformIO"
     , feedAuthorName  = "Jacob Stanley"
@@ -64,18 +75,6 @@ feedCfg = FeedConfiguration
 ------------------------------------------------------------------------
 
 type Page = H.Page String
-
--- Completely static.
-copy :: RulesM (Pattern CopyFile)
-copy = route idRoute >> compile copyFileCompiler
-
--- CSS directories
-css :: RulesM (Pattern String)
-css = route (setExtension "css") >> compile compressCssCompiler
-
--- Templates
-template :: RulesM (Pattern Template)
-template = compile templateCompiler
 
 -- Posts
 post :: RulesM (Pattern Page)
@@ -105,25 +104,18 @@ postRoute = dropHeadDir & setExtension "html" & dateDirs & dirWithIndex
 
 postFields :: Compiler Page Page
 postFields = arr
-    $ setIdentifier
+    $ setFieldFrom "url" "id" takeBaseName
     . setEscapedTitle
-    . setCleanUrl
+    . changeField "url" stripIndexPageName
     . setField "siteRoot" siteRoot
-
-setIdentifier :: Page -> Page
-setIdentifier p = setField "id" identifier p
-  where
-    identifier = takeBaseName (getField "url" p)
-
-setCleanUrl :: Page -> Page
-setCleanUrl p = setField "url" url p
-  where
-    url = cleanUrl (getField "url" p)
+    . copyBodyToField "description"
 
 setEscapedTitle :: Page -> Page
-setEscapedTitle p = setField "escapedTitle" title p
+setEscapedTitle = setFieldFrom "title" "escapedTitle" escape
   where
-    title = replaceAll "'" (const "\\'") (getField "title" p)
+    escape = replaceAll "'" (const "\\'")
+
+------------------------------------------------------------------------
 
 -- Top-level pages
 topLevel :: RulesM (Pattern Page)
@@ -138,16 +130,33 @@ topLevel = do
 topLevelFields :: Compiler Page Page
 topLevelFields =
         setFieldPostList recentFirst "allPosts"
-    >>> setFieldPostList (take numRecentPosts . recentFirst) "recentPosts"
     >>> setFieldPostList chronological "chronologicalPosts"
+    >>> setFieldPostTeasers (take numRecentPosts . recentFirst) "recentPosts"
 
 -- Create a post list based on ordering/selection
 setFieldPostList :: ([Page] -> [Page]) -> String -> Compiler Page Page
 setFieldPostList order key =
     setFieldPageList order "templates/post-item.html" key "posts/*"
 
+-- Create a post list based on ordering/selection
+setFieldPostTeasers :: ([Page] -> [Page]) -> String -> Compiler Page Page
+setFieldPostTeasers order key =
+    setFieldPageList order "templates/post-teaser.html" key "posts/*"
+
 ------------------------------------------------------------------------
 -- Utils
+
+-- Completely static.
+copy :: RulesM (Pattern CopyFile)
+copy = route idRoute >> compile copyFileCompiler
+
+-- CSS directories
+css :: RulesM (Pattern String)
+css = route (setExtension "css") >> compile compressCssCompiler
+
+-- Templates
+template :: RulesM (Pattern Template)
+template = compile templateCompiler
 
 withFields :: Compiler Page Page -> Compiler Resource Page
 withFields = pageCompilerWithFields parserState writerOptions id
@@ -158,6 +167,10 @@ parserState = defaultHakyllParserState
 writerOptions :: WriterOptions
 writerOptions = defaultHakyllWriterOptions
 
+-- | Maps a function over a field and stores it in a new field
+setFieldFrom :: String -> String -> (String -> String) -> Page -> Page
+setFieldFrom key key' f page = setField key' (f $ getField key page) page
+
 ------------------------------------------------------------------------
 -- URL mangling
 
@@ -165,13 +178,13 @@ cleanUrlCompiler :: Compiler Page Page
 cleanUrlCompiler = arr $ fmap $ mapAttrs clean
   where
     clean (key, value)
-      | key `S.member` urls = (key, cleanUrl value)
+      | key `S.member` urls = (key, stripIndexPageName value)
       | otherwise           = (key, value)
 
     urls = S.fromList ["src", "href"]
 
-cleanUrl :: String -> String
-cleanUrl url = fromMaybe url (stripSuffix "/index.html" url)
+stripIndexPageName :: String -> String
+stripIndexPageName url = fromMaybe url (stripSuffix "/index.html" url)
 
 stripSuffix :: Eq a => [a] -> [a] -> Maybe [a]
 stripSuffix suf xs = reverse `fmap` stripPrefix suf' xs'
